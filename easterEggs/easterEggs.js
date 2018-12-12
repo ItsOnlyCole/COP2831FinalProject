a40f87d14c100634ec5f53c124af5c63c76ee781
GamePlayerCole <ColeHemp@gmail.com>
2018-12-12 18:47:39 -0500

    Add Nav Link Highlight Class
    Added script to main.js to add a highlight:hover class to nav links.

---
+++ easterEggs/easterEggs.js
@@ -0,0 +1,13 @@
+/*  
+<div class="row">
+    <div class="col-sm-11">
+        <br>
+    </div>
+    <div class="secretButtonContainer col-sm-1">
+        <div class="secretButton">
+            <!--Empty div but on click will cause a series of events to occur-->
+            <br>
+        </div>
+    </div>
+</div>
+*/
